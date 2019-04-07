c3d9404c224d125ef0f632c523f215ec7ee132d8
Premal Samale <premaldattatray.samale@sjsu.edu>
2019-04-06 23:48:22 -0700

    converted to springboot project

---
+++ src/main/java/edu/sjsu/MainApp.java
@@ -0,0 +1,11 @@
+package edu.sjsu;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class MainApp extends SpringBootServletInitializer {
+	public static void main(String[] args) {
+	    SpringApplication.run(MainApp.class, args);
+	}
+}
